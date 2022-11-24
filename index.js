f0cca2d7235259fdd5378af875b801614654f94f
Maria <maricruzvelazquezmartinez01@gmail.com>
2022-11-24 15:10:33 -0300

    libreria-api(token)

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ app.use('/productos', require('./rutas/productos'));
 app.use('/usuario',require('./rutas/usuario'));
 app.use('/pedido',require('./rutas/pedido'));
 app.use('/detallesPedido', require('./rutas/detallesPedido'));
+app.use( '/sesion', require ('./rutas/sesion' ));
+
 
 app.listen(3000,()=>{
     console.log('El servidor inicio y esta escuchando en el puerto 3000.');
--- lista-usuario.js
+++ lista-usuario.js
@@ -1,7 +1,7 @@
 const USUARIO = [
-    {idusuario:1, nombre:"Maria", apellido:'Velazquez', direccion:"Nva Londres", telefono:0972893301, ci:6007188, digitoRuc:2, correo:"mariavela@gmail.com", password:1234567 },
-    {idusuario:2, nombre:"Fatima", apellido:'Arce', direccion:"Cnel.Oviedo", telefono:0982345301, ci:5467123, digitoRuc:3, correo:"fatyarce@gmail.com", password:7654321 },
-    {idusuario:3, nombre:"Maria", apellido:'ojeda', direccion:"Cnel. Oviedo", telefono:0971234561, ci:5234987, digitoRuc:7, correo:"mariaojeda@gmail.com", password:987654321 },
+    {idusuario:1, nombre:"Maria", apellido:'Velazquez', direccion:"Coronel Oviedo", telefono:0972893301, ci:6007188, digitoRuc:0, correo:"mariavela@gmail.com", password:12345678, idtipo: 1 },
+    {idusuario:2, nombre:"Fatima", apellido:'Arce', direccion:"Coronel Oviedo", telefono:0982345301, ci:5467123, digitoRuc:3, correo:"fatyarce@gmail.com", password:7654321, idtipo: 1 },
+    {idusuario:3, nombre:"Maria", apellido:'Ojeda', direccion:"Coronel Oviedo", telefono:0971234561, ci:5234987, digitoRuc:7, correo:"mariaojeda@gmail.com", password:987654321, idtipo: 1  },
 ]
 
 module.exports = USUARIO;
