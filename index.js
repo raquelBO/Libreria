5fba5f9d8a5feef1bd8be12e28cd9ec216ae089d
Raquel Ojeda <rachel_ojeda_b@hotmail.com>
2022-12-02 16:40:47 -0300

    Libreria-api(Actualizado)

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
-const app = express();
 const cors = require('cors');
 require('dotenv').config();
+const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
@@ -16,7 +16,7 @@ app.use('/productos', require('./rutas/productos'));
 app.use('/usuario',require('./rutas/usuario'));
 app.use('/pedido',require('./rutas/pedido'));
 app.use('/detallesPedido', require('./rutas/detallesPedido'));
-app.use( '/sesion', require ('./rutas/sesion' ));
+app.use('/sesion', require ('./rutas/sesion' ));
 
 
 app.listen(3000,()=>{
